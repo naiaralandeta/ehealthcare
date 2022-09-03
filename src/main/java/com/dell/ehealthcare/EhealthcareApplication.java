8399083d3eeebf72344277bc73691bc42420b3ef
Landeta,Naiara <Naiara_Landeta@Dell.com>
2022-09-03 09:37:08 +0100

    generate jar

--- src/main/java/com/dell/ehealthcare/EhealthcareApplication.java
+++ src/main/java/com/dell/ehealthcare/EhealthcareApplication.java
@@ -2,8 +2,10 @@ package com.dell.ehealthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EhealthcareApplication {
 
 	public static void main(String[] args) {
